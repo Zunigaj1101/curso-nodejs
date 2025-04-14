cca87712ea7a18a7db1bd4199dd824a5099040c5
Zunigaj1101 <josedz6102@gmail.com>
2025-04-13 20:55:32 -0400

    End of the sixth class

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import logger from 'morgan';
 import { Server } from "socket.io";
 import { createServer } from "http";
+import dotenv from "dotenv";
+import { createClient } from "@libsql/client";
+
+dotenv.config()
 
 const port = process.env.PORT ?? 1234
 
@@ -9,10 +13,28 @@ const app = express()
 const server = createServer(app) // Crear el servidor HTTP
 const io = new Server (server) // Integrar Socket.IO con el servidor HTTP
 
+const db = createClient ({
+    url: "libsql://solid-starbolt-zunigaj1101.aws-us-east-1.turso.io",
+    authToken: process.env.DB_TOKEN
+})
+
+try {
+    await db.execute (`
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            content TEXT,
+            date DATE,
+            user TEXT
+        )
+    `)
+} catch (error) {
+    console.log(error.message)
+}
+
 app.use(logger('dev'))
 
 // el Socket escucha la conexion de un usuario
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log ('a user has connected')
 
     // escucha la desconexión de un usario
@@ -21,9 +43,37 @@ io.on('connection', (socket) => {
     })
 
     // Escucha los mesaje de los usuarios
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+    socket.on('chat message', async (msg) => {
+        let result
+        const date = new Date().toISOString()
+        try {
+        result = await db.execute ({
+                sql: 'INSERT INTO messages (content, date) VALUES (:msg, :date)',
+                args: { msg, date}
+            }) 
+        } catch (error) {
+            console.error(error.message)
+            return
+        }
+
+        io.emit('chat message', msg, date ,result.lastInsertRowid.toString())
     })
+
+
+    if (!socket.recovered) {
+        try {
+            const results = await db.execute({
+                sql: 'SELECT id, content, date FROM messages WHERE id > ?',
+                args:[socket.handshake.auth.serverOffset ?? 0]
+            })
+            
+            results.rows.forEach (row => {
+                socket.emit('chat message', row.content, row.date, row.id.toString())
+            })
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
 })
 
 app.get('/',(req, res) => {
